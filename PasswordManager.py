5cfe85835b5c5ab0064c4d14870d68274995346c
aaronmunoz <aaronmunoz@pop-os.localdomain>
2022-12-26 16:52:33 -0500

    created & tested

---
+++ PasswordManager.py
@@ -0,0 +1,31 @@
+#This program is a password organizer
+#This will read, and write passwords
+#Will help rememering credentials for login interfaces
+
+
+def view():
+    with open('passwords.txt', 'r') as f: 
+        for line in f.readlines():
+            data = line.qrstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", passw)
+
+def add():
+    name = input('Account Name: ')
+    pwd = input ("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + '|' + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add)?, press q to quit?")
+    if mode == 'q':
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
